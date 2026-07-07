5702107b898962b8d81cdd5fd112699707ae01e7
phantuanhung2912 <phantuanhung2912@gmail.com>
2026-07-07 20:05:02 +0700

    thaydoi

---
+++ b.java
@@ -0,0 +1,6 @@
+public class b {
+    public static void main(String[] args) {
+        System.out.println("thân em trời trắng phận em tròn,");
+        System.out.println("bảy nổi ba chìm với nước non,");
+    }
+}
